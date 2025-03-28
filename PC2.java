4f16dca116c88512b3941bf781420c3affeaf305
Lorenzo-JGV <perezcarbonell.lorenzo@jgvaldemora.org>
2025-03-28 13:59:44 +0100

    Actividad modificada por Lorenzo - Develop

--- PC2.java
+++ PC2.java
@@ -4,6 +4,8 @@ public class PC2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo esta es mi segunda clase creada");
+		System.out.println("Línea añadida - Lorenzo");
+
 
 	}
 
